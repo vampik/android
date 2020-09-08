ce1e94008ecb1aa4db9fa66f0708d0d5c0e40353
Anna Tikhomirova <vamp@vampik.ru>
2020-09-08 21:11:17 +0300

    Do not discard subsequent regular location updates.

--- app/src/full/java/io/homeassistant/companion/android/sensors/LocationSensorManager.kt
+++ app/src/full/java/io/homeassistant/companion/android/sensors/LocationSensorManager.kt
@@ -231,7 +231,7 @@ class LocationSensorManager : BroadcastReceiver(), SensorManager {
         val now = System.currentTimeMillis()
 
         if (lastUpdateLocation == updateLocation.gps.contentToString()) {
-            if (now < lastLocationSend + 900000) {
+            if (now < lastLocationSend + 9000) {
                 Log.d(TAG, "Duplicate location received, not sending to HA")
                 return
             }
